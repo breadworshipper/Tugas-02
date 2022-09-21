fdf0432557ce33717cd1ec55631fe71d24c868ad
breadworshipper <azhrayashna@gmail.com>
2022-09-21 10:02:38 +0700

    Tugas 03 #1

--- tests.py
+++ tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase, Client
 from django.urls import resolve
 
+# Test tiga url mywatchlist
 class AppTest(TestCase):
     def test_html(self):
-        response = Client().get('/mywatchlist')
+        response = Client().get('/mywatchlist', follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_xml(self):
-        response = Client().get('/mywatchlist/xml')
+        response = Client().get('/mywatchlist/xml', follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_json(self):
-        response = Client().get('/mywatchlist/json')
+        response = Client().get('/mywatchlist/json', follow=True)
         self.assertEqual(response.status_code, 200)
